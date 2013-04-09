7cc4c87987e7e6a5bf83b664503a9bad304dfef6
Pablo O Vieira <povieira@gmail.com>
2013-04-09 15:55:37 -0300

    test case

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# setup
+# Copyright (c) 2013 Pablo O Vieira (povieira)
+# See README.md and LICENSE.md for details.
+
 from setuptools import setup, find_packages
 from noipy import __version__, __author__, __email__, __license__
 
@@ -14,11 +18,24 @@ setup(
         author_email=__email__,
         url='https://github.com/povieira/noipy',
         packages=find_packages(),
-        keywords='no-ip ddns update api',
+        keywords=['no-ip', 'ddns', 'api'],
+        classifiers = [
+            'Development Status :: 4 - Beta',
+            'Environment :: Other Environment',
+            'Intended Audience :: Developers',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: Eclipse Public License (EPL)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Internet :: Name Service (DNS)',
+        ],
         entry_points={
             'console_scripts': [
                     'noipy = noipy.dnsupdater:main',
             ],
         },
-        zip_safe=True
+        zip_safe=True,
+        test_suite='test.test_noipy'
 )
---
+++ test/test_noipy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# tests
+# Copyright (c) 2013 Pablo O Vieira (povieira)
+# See README.md and LICENSE.md for details.
+
+import unittest
+import re
+from noipy import dnsupdater
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_get_ip(self):
+        ip = dnsupdater.get_ip()
+        valid_ip_regex = '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
+        self.assertTrue(re.match(valid_ip_regex, ip), 'get_ip() failed.')
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
