d0388dc5a422a53b2949e72ae52d62bf346b3e45
Pablo O Vieira <povieira@gmail.com>
2013-04-09 09:13:23 -0300

    code standardization and organization

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
 from noipy import __version__, __author__, __email__, __license__
 
 setup(
-        name="noipy",
+        name='noipy',
         version=__version__,
-        description="Command line update for No-IP DDNS Update API",
+        description='Command line update for No-IP DDNS Update API',
         long_description=open('README.md').read(),
         license=__license__,
         author=__author__,
         author_email=__email__,
-        url="https://github.com/povieira/noipy",
+        url='https://github.com/povieira/noipy',
         packages=find_packages(),
-        keywords="no-ip ddns update api",
+        keywords='no-ip ddns update api',
         entry_points={
             'console_scripts': [
                     'noipy = noipy.dnsupdater:main',
