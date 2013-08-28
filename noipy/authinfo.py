b7827ccd7e64f884b1d3cb2106705c61512aa745
Pablo O Vieira <povieira@gmail.com>
2013-08-28 09:44:10 -0300

    bug fix

--- noipy/authinfo.py
+++ noipy/authinfo.py
@@ -87,6 +87,6 @@ def load(provider, auth_dir=AUTHFILE_DIR):
 
     return auth
 
-def exists(provider):
-    auth_file = os.path.join(AUTHFILE_DIR, provider)
+def exists(provider, auth_dir=AUTHFILE_DIR):
+    auth_file = os.path.join(auth_dir, provider)
     return os.path.exists(auth_file)
